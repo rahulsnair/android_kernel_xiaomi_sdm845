bc6d6f91e82afcd78a90806398b6583158629f7e
Bruno Martins <bgcngm@gmail.com>
2018-06-23 00:49:08 +0200

    drivers: power: supply: Import Xiaomi changes
    Change-Id: I0860cb19093b7fe1c3f416b7804347bd6a1ba80a

--- drivers/power/supply/qcom/fg-core.h
+++ drivers/power/supply/qcom/fg-core.h
@@ -96,6 +96,10 @@ enum prof_load_status {
 	PROFILE_SKIPPED,
 	PROFILE_NOT_LOADED,
 };
+#define VBAT_RESTART_FG_EMPTY_UV		3700000
+#define TEMP_THR_RESTART_FG		150
+#define RESTART_FG_START_WORK_MS		1000
+#define RESTART_FG_WORK_MS		2000
 
 /* Debug flag definitions */
 enum fg_debug_flag {
@@ -342,6 +346,7 @@ struct fg_batt_props {
 	int		float_volt_uv;
 	int		vbatt_full_mv;
 	int		fastchg_curr_ma;
+	int		nom_cap_uah;
 };
 
 struct fg_cyc_ctr_data {
@@ -493,6 +498,7 @@ struct fg_chip {
 	enum prof_load_status	profile_load_status;
 	bool			battery_missing;
 	bool			fg_restarting;
+	bool			empty_restart_fg;
 	bool			charge_full;
 	bool			recharge_soc_adjusted;
 	bool			ki_coeff_dischg_en;
@@ -503,6 +509,7 @@ struct fg_chip {
 	bool			use_ima_single_mode;
 	bool			usb_present;
 	bool			twm_state;
+	bool			report_full;
 	bool			use_dma;
 	bool			qnovo_enable;
 	bool			suspended;
@@ -512,11 +519,14 @@ struct fg_chip {
 	struct work_struct	status_change_work;
 	struct work_struct	esr_sw_work;
 	struct delayed_work	ttf_work;
+	struct delayed_work	esr_timer_config_work;
 	struct delayed_work	sram_dump_work;
+	struct delayed_work	soc_work;
 	struct delayed_work	pl_enable_work;
 	struct work_struct	esr_filter_work;
 	struct alarm		esr_filter_alarm;
 	ktime_t			last_delta_temp_time;
+	struct delayed_work	empty_restart_fg_work;
 };
 
 /* Debugfs data structures are below */
